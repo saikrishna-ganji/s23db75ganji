7f43112561915a94f9fb9719895be15f1f616a63
Saikrishna.ganji <saikrishnaganji7@gmail.com>
2023-04-05 16:26:18 -0500

    added board and updated app js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var buds = require('./routes/earphones');
+var boardRoute = require('./routes/board');
 
 var app = express();
 
@@ -19,8 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/earphones', buds);
+app.use('/board',boardRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
