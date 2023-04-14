9137bca0ebe521a3614b88404a7d66e4c1683139
Saikrishna.ganji <saikrishnaganji7@gmail.com>
2023-04-13 23:49:48 -0500

    final commit

--- controllers/earphones.js
+++ controllers/earphones.js
@@ -44,3 +44,23 @@ exports.earphones_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle earphones create on POST.
+exports.earphones_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new earphones();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    document.earphone_name = req.body.earphone_name;
+    document.earphone_shape = req.body.earphone_shape;
+    document.earphone_radius = req.body.earphone_radius;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
