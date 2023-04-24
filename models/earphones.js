f92e018381e6a8b4986000bd2fe6cc9f19592458
Saikrishna.ganji <saikrishnaganji7@gmail.com>
2023-04-23 22:10:46 -0500

    final  commit

--- models/earphones.js
+++ models/earphones.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const earphonesSchema = mongoose.Schema({
-earphone_name: String,
-earphone_shape: String,
-earphone_radius: Number
+earphone_name: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: [4, "Earphone name is not valid"],
+  },
+earphone_shape: {
+    type: String,
+    required: true,
+    minLength: [1, "Earphone Shape is not valid"],
+  },
+earphone_radius: {
+    type: Number,
+    required: true,
+    min: [2, "Earphone Radius is nt valid"],
+  },
 })
 module.exports = mongoose.model("earphones",
 earphonesSchema)
