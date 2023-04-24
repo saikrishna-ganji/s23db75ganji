e849ca1e7222bfa832739aa158fbb7f6e5cab117
Saikrishna.ganji <saikrishnaganji7@gmail.com>
2023-04-23 22:10:34 -0500

    final commit

--- routes/earphones.js
+++ routes/earphones.js
@@ -1,6 +1,17 @@
 var express = require('express');
-const earphones_controlers= require('../controllers/earphones');
+const earphones_controlers = require('../controllers/earphones');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET earphones */
 router.get('/', earphones_controlers.earphones_view_all_Page);
@@ -12,7 +23,7 @@ router.get('/detail', earphones_controlers.earphones_view_one_Page);
 router.get('/create', earphones_controlers.earphones_create_Page);
 
 /* GET create update page */
-router.get('/update', earphones_controlers.earphones_update_Page);
+router.get('/update', secured, earphones_controlers.earphones_update_Page);
 
 /* GET delete earphones page */
 router.get('/delete', earphones_controlers.earphones_delete_Page);
